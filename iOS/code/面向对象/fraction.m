03ce01fcc1162634c9a9a05aeab3fb1e408aefb3
ChrisPan <39005916+szupzj18@users.noreply.github.com>
2020-05-23 12:55:33 +0800

    change

---
+++ iOS/code/面向对象/fraction.m
@@ -0,0 +1,53 @@
+#import <Foundation/Foundation.h>、
+/*
+OC面向对象
+经典分数实例
+*/
+
+//----------- interface部分 -------------
+@interface Fraction: NSObject
+
+-(void) print;
+-(void) setNumerator;
+-(void) setDenominator:(int)n;
+
+@end
+//---------- implementation部分 ---------
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+-(void)print
+{
+    NSLog(@"%i/%i",numerator,denominator);
+}
+-(void)setNumerator:(int) n
+{
+    numerator = n;
+}
+-(void)setDenominator:(int) n
+{
+    denominator = n;
+}
+@end
+//----------program部分---------------
+int main(int argc, const char *argv[]){
+    @autoreleasepool{
+        Fraction *mFraction;
+
+        //创建实例
+        mFraction = [Fraction alloc];
+        mFraction = [Fraction init];
+
+        //设置分子分母
+        [mFraction setNumerator:1];
+        [mFraction setDenominator:3];
+
+        NSLog(@"The value of mFraction is ")
+        [mFraction print];
+
+        //NSLog(@"hello world!");
+    }
+    return 0;
+}
